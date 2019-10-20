cf9cbb4b0f6625de2d437de0aa44632a327bf219
Mukul Joshi <joshimukul29@gmail.com>
2019-10-20 13:48:52 +0530

    Added Approximate Time Synchronizer for subscribing to both proximity sensors and current position of quadricopter

--- src/quadricopterController.py
+++ src/quadricopterController.py
@@ -2,16 +2,20 @@
 import rospy
 from geometry_msgs.msg import Pose
 import math
+from std_msgs.msg import Bool
+import message_filters
 
 # Attributes
 translationStep = 0.01
 rotationStep = 0.01
 
 targetPosition = Pose()
-gpsPublisher = rospy.Publisher('gpsToVREP', Pose, queue_size=100)
-def gpsCallback(msg):
-    targetPosition = msg
+gpsPub = rospy.Publisher('gpsToVREP', Pose, queue_size=100)
+def callback(gps, proximitySensorLeft, proximitySensorRight):
+    targetPosition = gps
     print targetPosition
+    print proximitySensorLeft
+    print proximitySensorRight
 
     # Move the quadricopter forward 
     # This forward movement will be required later
@@ -23,12 +27,17 @@ def gpsCallback(msg):
     ###############################################
 
 
-    gpsPublisher.publish(targetPosition)
+    gpsPub.publish(targetPosition)
     print targetPosition
 
 def controller():
     rospy.init_node('connectionToVREP', anonymous=True)
-    gpsSubscriber = rospy.Subscriber('gpsToROS', Pose, gpsCallback)
+    gpsSub = message_filters.Subscriber('gpsToROS', Pose)
+    proximitySensorLeftSub = message_filters.Subscriber('proximitySensorLeft', Bool)
+    proximitySensorRightSub = message_filters.Subscriber('proximitySensorRight', Bool)
+
+    approximateTimeSyncronizer = message_filters.ApproximateTimeSynchronizer([gpsSub, proximitySensorLeftSub, proximitySensorRightSub], queue_size=10, slop=0.1, allow_headerless=True)
+    approximateTimeSyncronizer.registerCallback(callback)
     rospy.spin()
 
 if __name__ == '__main__':
